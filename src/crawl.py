9834c6b87121c41a2917c3eee048e9fec4ca534c
Narika <drnarika@outlook.com>
2024-09-28 22:05:04 +0800

    First Commit

---
+++ src/crawl.py
@@ -0,0 +1,126 @@
+from bs4 import BeautifulSoup as BS
+import yaml
+import requests
+
+class Source:
+    def __init__(self,path: str) -> None:
+        self.loadFromYAML(path)
+    
+    def loadFromYAML(self,path: str):
+        with open(path,'r',encoding='utf-8')as r:
+            source = yaml.load(r,Loader=yaml.FullLoader)
+            if source['enabled'] == False:
+                print(f'Source {source["title"]} has been skipped.')
+                self.ENABLED = False
+                return 0
+            else: self.ENABLED = True
+            print(f'Start loading source {source["title"]}.')
+            for key in source.keys():
+                if key == 'title':
+                    self.TITLE = source['title']
+                elif key == 'link':
+                    self.LINK = source['link']
+                elif key == 'author':
+                    self.AUTHOR = source['author']
+                elif key == 'next-page':
+                    self.NEXT_PAGE = source['next-page']
+                elif key == 'category':
+                    self.updateCategories(source['category'])
+                elif key == 'item':
+                    self.ITEM_PREF = source['item']
+                elif key == 'content':
+                    self.CONTENT_PREF = source['content']
+            print(f'Loading {self.TITLE} completed.\n')
+                    
+    def updateCategories(self,categories:list):
+        t_categories = []
+        for c in categories:
+            t_c = Category(self, c)
+            t_categories.append(t_c)
+        self.CATEGORY = t_categories
+        del(t_categories)
+        
+    items = []
+    def update(self):
+        for c in self.CATEGORY:
+            print(f'Start updating category {c.TITLE}.')
+            items_category = c.updateItems()
+            self.items.extend(items_category)
+            print(f'Updating {c.TITLE} completed.')
+            
+    
+    iteration = 0
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if len(self.items)-1<self.iteration:
+            raise StopIteration
+        i = self.items[self.iteration]
+        self.iteration += 1
+        return i
+            
+class Category:
+    
+    def __init__(self,source:Source,data:list) -> None:
+        self.TITLE = data['title']
+        self.LINK = f'{source.LINK}{data["link"]}'
+        self.SOURCE = source
+    
+    def updateItems(self):
+        t_items = []
+        now_link = self.LINK
+        while True:
+            req = Request(now_link)
+            bs = BS(req.ask(),features='lxml')
+            for box in bs.select(self.SOURCE.ITEM_PREF['box']):
+                t_items.append(Item(self.SOURCE,BS(str(box),features="lxml"),self))
+            next = bs.select_one(self.SOURCE.NEXT_PAGE.split('$')[0])
+            if next != None:
+                now_link = next.attrs[self.SOURCE.NEXT_PAGE.split('$')[1]]
+                continue
+            else:
+                break
+        self.ITEM = t_items
+        del(t_items)
+        self.fetchContent()
+        return self.ITEM
+    
+    def fetchContent(self):
+        for item in self.ITEM:
+            item.fetchContent()
+            
+class Item:
+    def __init__(self,source:Source,box:BS,parent:Category) -> None:
+        if source.ITEM_PREF['title'].split('$')[1] != 'innerText':
+            self.TITLE = box.select_one(source.ITEM_PREF['title'].split('$')[0]) \
+                .attrs[source.ITEM_PREF['title'].split('$')[1]]
+        else:
+            self.TITLE = box.select_one(source.ITEM_PREF['title'].split('$')[0]).getText()
+        self.LINK = box.select_one(source.ITEM_PREF['link'].split('$')[0]) \
+            .attrs[source.ITEM_PREF['link'].split('$')[1]]
+        self.PUBDATE = box.select_one(source.ITEM_PREF['pubDate'].split('$')[0]) \
+            .attrs[source.ITEM_PREF['pubDate'].split('$')[1]]
+        self.SOURCE = source
+        self.BOX = box
+        self.PARENT = parent
+    
+    def fetchContent(self):
+        req = Request(self.LINK)
+        self.CONTENT = str(BS(req.ask(),features='lxml').select_one(self.SOURCE.CONTENT_PREF['box']))
+        print(f'Fetching {self.TITLE} completed.\r')
+    
+class Request:
+    USER_AGENT = r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0'
+    
+    def __init__(self,link:str) -> None:
+        self.LINK = link
+        # self.REFERER = referer
+    
+    def ask(self):
+        HEADER = {'User-Agent': self.USER_AGENT,'Rederer':self.LINK}
+        try:
+            req = requests.get(self.LINK,headers=HEADER)
+        except Exception as e:
+            print(f'Error when request: {e}')
+        return req.text
---
+++ src/rss.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup as BS
+import yaml,os
+from crawl import Source, Category, Item
+
+class RSSConstructer:
+    
+    def __init__(self,source:Source) -> None:
+        self.SOURCE = source
+        self.document = BS(features='xml')
+        self.initRSSRootNode()
+        
+    def initRSSRootNode(self):
+        rss = self.document.new_tag('rss',attrs={'version':'2.0'})
+        self.document.append(rss)
+        channel = self.document.new_tag('channel')
+        rss.append(channel)
+        self.root_node = rss
+        self.channel_node = channel
+        
+    def parse(self):
+        title_node = self.document.new_tag('title')
+        title_node.string = self.SOURCE.TITLE
+        self.channel_node.append(title_node)
+        
+        link_node = self.document.new_tag('link')
+        link_node.string = self.SOURCE.LINK
+        self.channel_node.append(link_node)
+        
+        author_node = self.document.new_tag('author')
+        author_node.string = self.SOURCE.AUTHOR
+        self.channel_node.append(author_node)
+        
+        items = iter(self.SOURCE)
+        print('Serializing content into RSSXml...\n')
+        for item in items:
+            item_node = self.document.new_tag('item')
+            
+            item_title = self.document.new_tag('title')
+            item_title.string = item.TITLE
+
+            item_link = self.document.new_tag('link')
+            item_link.string = item.LINK
+
+            item_category = self.document.new_tag('category')
+            item_category.string = item.PARENT.TITLE
+
+            item_description = self.document.new_tag('description')
+            item_description.insert(0,item.CONTENT)
+
+            item_pubDate = self.document.new_tag('pubDate')
+            item_pubDate.string = item.PUBDATE
+
+            
+            item_node.append(item_title)
+            item_node.append(item_link)
+            item_node.append(item_category)
+            item_node.append(item_description)
+            item_node.append(item_pubDate)
+            
+            self.channel_node.append(item_node)
+            print(f'Serializing {item.TITLE} completed.\r')
+    
+    def export(self,desPath:str):
+        print(f'Export result to {desPath}...')
+        with open(desPath,'w+')as w:
+            w.write(self.document.prettify('utf-8').decode(errors='ignore'))
+        print(f'Export completed. Exported {len(self.SOURCE.items)} items.')
+        
